35741165065b110e4717afe8578aa938d7257afc
jorted-runner <jorted.python@gmail.com>
2025-03-11 14:02:13 -0500

    Dynamic arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,17 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // create a list of doubles
+        List<double> result = new List<double>();
+
+        // loop through from 1 to the length variable
+        for (int i = 1; i <= length; i++) {
+            // add the number * i to the list
+            result.Add(number * i);
+        }
+
+
+        return result.ToArray();
     }
 
     /// <summary>
@@ -29,5 +39,20 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Find the index for where the new list will begin
+        int splitIndex = data.Count - amount; 
+
+        // Slice the list into two parts.
+        //Get the new start by starting at the split index and getting the shift amount
+        List<int> newStart = data.GetRange(splitIndex, amount);
+
+        // get the remaining elements up to the split
+        List<int> newEnd = data.GetRange(0, splitIndex);
+
+        // clear out the original list. Add the new start then add the new end.
+        data.Clear();
+        data.AddRange(newStart);
+        data.AddRange(newEnd);
     }
 }
