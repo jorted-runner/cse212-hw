02b32c3e9f70ed2a50c9eb52184f237538bb2471
jorted-runner <jorted.python@gmail.com>
2025-04-01 12:06:20 -0500

    Team activity Hash Sets

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,4 +1,6 @@
-﻿public static class DisplaySums {
+﻿using System.Reflection;
+
+public static class DisplaySums {
     public static void Run() {
         DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         // Should show something like (order does not matter):
@@ -29,5 +31,31 @@
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+
+        // My solution
+        // var used = new HashSet<int>();
+
+        // foreach(var number1 in numbers) {
+        //     foreach (var number2 in numbers) {
+        //         if (number1 + number2 == 10 && !used.Contains(number2) && !used.Contains(number1) && number1 != number2) {
+        //             used.Add(number1);
+        //             used.Add(number2);
+        //             Console.Write(number1);
+        //             Console.Write(" ");
+        //             Console.Write(number2);
+        //             Console.Write("\n");
+        //         }
+        //     }
+        // }
+
+        // Better solution
+        var usedNums = new HashSet<int>();
+        foreach (var n in numbers) {
+            if (usedNums.Contains(10 - n)) {
+                Console.WriteLine($"{n} {10 - n}");
+            }
+            usedNums.Add(n);
+        }
+
     }
 }
